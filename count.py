c141873342e3c7a3e9f224e856cf1046ef2197eb
Evgeny Marchenko <3.marchenko@gmail.com>
2020-09-12 12:48:46 -0400

    some fixes

--- count.py
+++ count.py
@@ -1,21 +1,21 @@
 def count(sequence, item):
-    #print sequence
-    #print item
-    #print sequence.count(item)
-
+    # print sequence
+    # print item
+    # print sequence.count(item)
 
     counts = 0
     new_string = ''
 
     for i in sequence:
-        #print i
-        #print y
+        # print i
+        # print y
         new_string = new_string + str(i)
-        #print sequence[i]
-        #if i == item:
+        # print sequence[i]
+        # if i == item:
         #    counts += 1
         if item == i:
             counts += 1
     return counts
 
-print count([4,2,1,1,1,1,1], 1)
+
+print(count([4, 2, 1, 1, 1, 1, 1], 1))
--- coursera_biggest_and_smallest.py
+++ coursera_biggest_and_smallest.py
@@ -2,7 +2,7 @@ largest = None
 smallest = None
 while True:
 
-    number = raw_input('Enter number')
+    number = input('Enter number ')
     if number == 'done':
         break
     else:
@@ -10,23 +10,17 @@ while True:
             number = int(number)
 
         except:
-            print "Invalid input"
+            print("Invalid input")
             continue
 
     if smallest is None:
         smallest = number
+    elif largest is None:
+        largest = number
     elif number < smallest:
         smallest = number
     elif number > largest:
         largest = number
 
-
-
-
-
-print "Maximum is", largest
-print "Minimum is", smallest
-
-
-
-
+print("Maximum is", largest)
+print("Minimum is", smallest)
